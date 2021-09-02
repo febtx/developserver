a026596fca9ae92beee3d25a74d0f9a6288c8f5f
Nguyễn Văn Khoa <kizzroyal@gmail.com>
2021-09-02 15:22:14 +0700

    update

--- app/Cron/taixiu.js
+++ app/Cron/taixiu.js
@@ -129,7 +129,7 @@ let TopHu = function(){
 				delete obj._id;
 				return obj;
 			});
-			let configSystem = Helpers.getConfig('sys');
+			let taixiujs = Helpers.getConfig('sys');
 			let temp_data = {TopHu:{
 				mini_poker: result.filter(function(mini_poker){
 					return (mini_poker.game === 'minipoker')
@@ -139,15 +139,6 @@ let TopHu = function(){
 				}),
 				vq_red: result.filter(function(vq_red){
 					return (vq_red.game === 'vuongquocred')
-				}),
-				audition: result.filter(function(audition){
-					return (audition.game === 'audition')
-				}),
-				lankwaifong: result.filter(function(lankwaifong){
-					return (lankwaifong.game === 'lankwaifong')
-				}),
-				angrybirdslot: result.filter(function(angrybirdslot){
-					return (angrybirdslot.game === 'AngryBirdSlot')
 				}),
 				caothap: result.filter(function(caothap){
 					return (caothap.game === 'caothap')
@@ -167,7 +158,7 @@ let TopHu = function(){
 				megaj: result.filter(function(megaj){
 					return (megaj.game === 'megaj')
 				}),
-				socialnetwork: configSystem
+				socialnetwork: taixiujs
 			}};
 			io.broadcast(temp_data);
 		}
@@ -576,9 +567,11 @@ let playGame = function(){
 					botList = [...io.listBot];
 					
 					let maxBot = (botList.length*90/100)>>0;
+					let maxBottem = (botList.length*10/100)>>0;
 					botList = Helpers.shuffle(botList); // tráo
 					botList = botList.slice(0, maxBot);
-					botListChat = botTemp;
+					botTemp = botTemp.slice(0, maxBottem);
+					//botListChat = botTemp;
 					maxBot = null;
 				}else{
 					botTemp = [];
